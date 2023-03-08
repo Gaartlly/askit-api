106f139d7f208952889aae4bd1493158bb439f51
mateus-mos <mateus.silvaoliveira13@outlook.com>
2023-03-08 09:52:49 -0300

    [CHANGE] comment controller name and createComment route is now working

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express';
 import { PrismaClient, Prisma } from '@prisma/client';
 import * as z from 'zod';
+import { any, string } from 'zod';
 
 const prisma = new PrismaClient();
+
+const createCommentSchema = z.object({
+  text: string(),
+  category: string(),
+  postId: string(),
+  parentCommentId: string(),
+  files: any() 
+});
 
 /**
  * Get all comments for a particular post.
@@ -32,21 +41,21 @@ export const getComments = async (req: Request, res: Response): Promise<void> =>
  * @returns {Promise<void>}
  */
 export const createComment = async (req: Request, res: Response): Promise<void> => {
-    try {
-        // create comment without files
-        // create files
-        // update comment with files 
-        const { text, category, postId, parentCommentId, files } = req.body;
+  try {
+    const { text, category, files } = createCommentSchema.parse(req.body); 
+    const postId = parseInt(createCommentSchema.parse(req.body).postId);
+    const parentCommentId = parseInt(createCommentSchema.parse(req.body).postId);
 
-        const comment =  await prisma.comment.create({
-            data:{
-                text,
-                category,
-                postId,
-                parentCommentId
-            }
-        });
-        
-    } catch (error) {
-    }
+    const comment =  await prisma.comment.create({
+      data:{
+          text,
+          category,
+          postId,
+          parentCommentId
+      }
+    });
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error: error});
+  }
 };
