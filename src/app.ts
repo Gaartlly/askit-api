2d45a98d3940c863e4403a400a3783e6679bcfd9
mateus-mos <mateus.silvaoliveira13@outlook.com>
2023-03-08 09:54:10 -0300

    [ADD] Comment routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { createUser, getUsers } from "./controllers/UserController";
+import commentRoutes from "./routes/commentRoutes"
+
 const app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.get("/getUsers", getUsers);
-app.post("/createUser", createUser);
 
 app.get("/", (_, res) => {
   res.send("Script created by Matheus Souza.");
 });
 
 app.listen(port, () => {
-console.log(`App running at http://localhost:${port}`);
+  console.log(`App running at http://localhost:${port}`);
 });
 
+/**
+ * App routes
+ */
+app.use('/api/comments', commentRoutes);
