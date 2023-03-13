825a3b3e7dbbf7c9e983886104aaa4a2226c28f3
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-03-13 05:52:29 -0300

    [FIX] Update function with unnecessary file deletions

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 const prisma = new PrismaClient();
 
 export const create = async (req: Request, res: Response) => {
-	const schema = z.object({
+	const createSchema = z.object({
 		title: z.string().min(1).max(255),
 		description: z.string().min(1).max(255),
 		upvotes: z.number().int().default(0),
@@ -30,19 +30,19 @@ export const create = async (req: Request, res: Response) => {
 	});
 
 	try {
-		const { title, description, upvotes, downvotes, authorId, tags, files } = schema.parse(req.body);
-		const post = await prisma.post.create({
+		const post = createSchema.parse(req.body);
+		const createdPost = await prisma.post.create({
 			data: {
-				title,
-				description,
-				upvotes,
-				downvotes,
-				authorId,
+				title: post.title,
+				description: post.description,
+				upvotes: post.upvotes,
+				downvotes: post.downvotes,
+				authorId: post.authorId,
 				tags: {
-					create: tags,
+					create: post.tags,
 				},
 				files: {
-					create: files,
+					create: post.files,
 				},
 			},
 			include: {
@@ -50,16 +50,14 @@ export const create = async (req: Request, res: Response) => {
 				files: true,
 			},
 		});
-		res.status(200).json({ message: "Post created.", post });
+		res.status(200).json({ message: "Post created.", post: createdPost });
 	} catch (err) {
 		res.status(500).json({ message: "Internal server error.", err: err.message });
 	}
 };
 
 export const update = async (req: Request, res: Response) => {
-	const { id } = req.params;
-
-	const postSchema = z.object({
+	const updateSchema = z.object({
 		title: z.string().min(1).max(255).optional(),
 		description: z.string().min(1).max(255).optional(),
 		upvotes: z.number().int().optional(),
@@ -68,6 +66,7 @@ export const update = async (req: Request, res: Response) => {
 		tags: z
 			.array(
 				z.object({
+					id: z.number().int().optional(),
 					key: z.string().min(1).max(255),
 					category: z.string().min(1).max(255),
 				})
@@ -76,6 +75,7 @@ export const update = async (req: Request, res: Response) => {
 		files: z
 			.array(
 				z.object({
+					id: z.number().int().optional(),
 					title: z.string().min(1).max(255),
 					path: z.string().min(1).max(255),
 				})
@@ -84,22 +84,44 @@ export const update = async (req: Request, res: Response) => {
 	});
 
 	try {
-		const { title, description, upvotes, downvotes, authorId, tags, files } = postSchema.parse(req.body);
-		const post: Post = await prisma.post.update({
+		const { id } = req.params;
+		const currentPost = await prisma.post.findUnique({
+			where: { id: Number(id) },
+			include: {
+				tags: true,
+				files: true,
+			},
+		});
+
+		if (currentPost === null) {
+			res.status(404).json({ message: `Post ${id} not found` });
+		}
+
+		const currentFilesIds = currentPost.files.map((file) => file.id);
+		const newPost = updateSchema.parse(req.body);
+		const createdFiles = newPost.files.filter((obj) => obj.id == undefined); //Arquivos que chegam sem id
+		const updatedFiles = newPost.files.filter((obj) => obj.id !== undefined); //Arquivos que chegam com id
+		const deletedFilesIds = currentFilesIds.filter((id) => !updatedFiles.map((obj) => obj.id).includes(id)); //Arquivos que não estão entre os novos
+
+		await prisma.file.deleteMany({ where: { id: { in: deletedFilesIds } } });
+		for (const file of updatedFiles) {
+			await prisma.file.update({ where: { id: file.id }, data: file });
+		}
+
+		const updatedPost: Post = await prisma.post.update({
 			where: { id: Number(id) },
 			data: {
-				title,
-				description,
-				upvotes,
-				downvotes,
-				authorId,
+				title: newPost.title,
+				description: newPost.description,
+				upvotes: newPost.upvotes,
+				downvotes: newPost.downvotes,
+				authorId: newPost.authorId,
 				tags: {
 					deleteMany: {},
-					create: tags,
+					createMany: { data: newPost.tags },
 				},
 				files: {
-					deleteMany: {},
-					create: files,
+					createMany: { data: createdFiles },
 				},
 			},
 			include: {
@@ -107,8 +129,9 @@ export const update = async (req: Request, res: Response) => {
 				files: true,
 			},
 		});
-		res.status(200).json({ message: "Post updated.", post });
+		res.status(200).json({ message: "Post updated.", post: updatedPost });
 	} catch (err) {
+		console.error(err);
 		res.status(500).json({ message: "Internal server error.", err: err.message });
 	}
 };
@@ -142,7 +165,7 @@ export const show = async (req: Request, res: Response) => {
 		if (post === null) {
 			res.status(404).json({ message: `Post ${id} not found` });
 		} else {
-			res.json(post);
+			res.json({ post: post });
 		}
 	} catch (err) {
 		res.status(500).json({ message: "Internal server error.", err: err.message });
