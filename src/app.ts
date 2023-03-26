309694d23e655da39160f00c6ea6d544999fc797
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-03-26 15:22:55 -0300

    [ADD] Importation of new routes

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ import fileRoutes from './routes/fileRoutes';
 import postRoutes from './routes/postRoutes';
 import userRoutes from './routes/userRoutes';
 import tagRoutes from './routes/tagRoutes';
+import reactionRoutes from './routes/reactionRoutes';
+import reportRoutes from './routes/reportRoutes';
+import userControlRoutes from './routes/userControlRoutes';
 import { verifyAuthentication } from './middleware/authUserMiddleware';
 import loginRoutes from './routes/loginRoute';
 import swaggerUi from 'swagger-ui-express';
@@ -18,7 +21,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 /**
  * App routes
@@ -28,8 +31,10 @@ app.use('/api/file', verifyAuthentication, fileRoutes);
 app.use('/api/comment', verifyAuthentication, commentRoutes);
 app.use('/api/post', verifyAuthentication, postRoutes);
 app.use('/api/tag', verifyAuthentication, tagRoutes);
+app.use('/api/reaction', verifyAuthentication, reactionRoutes);
+app.use('/api/report', verifyAuthentication, reportRoutes);
+app.use('/api/userControl', verifyAuthentication, userControlRoutes);
 app.use('/api', loginRoutes);
-
 
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
