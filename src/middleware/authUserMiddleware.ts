6a49af9326874de4d7c99f36965d5d5ea23ed299
mateus-mos <mateus.silvaoliveira13@outlook.com>
2023-03-26 16:20:02 -0300

    [CHANGE] to the new error handler pattern

--- src/middleware/authUserMiddleware.ts
+++ src/middleware/authUserMiddleware.ts
@@ -1,19 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { UnauthorizedError } from '../utils/responseHandler';
+import { asyncHandler } from '../utils/responseHandler';
 import { secretKey } from '../utils/tokenJwtUtil';
 
-export const verifyAuthentication = async (req: Request, res: Response, next: NextFunction) => {
+export const verifyAuthentication = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
-    if (!token) return res.status(401).json({ message: 'Token is missing!' });
+    if (!token)
+        throw new UnauthorizedError('Authentication token not found!'); 
 
     // Bearer token
     const [_, jwtToken] = token.split(' ');
 
-    try {
-        jwt.verify(jwtToken, secretKey);
-        return next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid token!' });
-    }
-};
+    jwt.verify(jwtToken, secretKey);
+    return next();
+});
--- src/services/tokenJwtService/generateTokenJwt.ts
+++ src/services/tokenJwtService/generateTokenJwt.ts
@@ -1,8 +1,9 @@
 import { secretKey } from '../../utils/tokenJwtUtil';
 import { sign } from 'jsonwebtoken';
+import { asyncHandler, InternalServerError } from '../../utils/responseHandler';
 
-const generateJwtToken = async (userId: number) => {
-    const expiresIn = 20; // 20 seconds for tests
+const generateJwtToken = asyncHandler(async (userId: number) => {
+    const expiresIn = 10000; // 20 seconds for tests
 
     const tokenJwt = sign({}, secretKey, {
         subject: `${userId}`,
@@ -10,9 +11,9 @@ const generateJwtToken = async (userId: number) => {
         algorithm: 'HS256',
     });
 
-    if (!tokenJwt) throw new Error('Unable to create token JWT');
+    if (!tokenJwt) throw new InternalServerError('Unable to create token JWT');
 
     return tokenJwt;
-};
+});
 
 export default generateJwtToken;
