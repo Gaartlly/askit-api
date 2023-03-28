0058597f072b5ea2fafa80ca6fe2b82b71054efe
devmatheussouza <matheus-oliveira17@hotmail.com>
2023-03-28 14:19:23 -0300

    Types added to prisma client returns.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,18 @@
 import { Response, Request } from 'express';
-import { Role } from '@prisma/client';
+import { Course, Role, User } from '@prisma/client';
 import { z } from 'zod';
 import { hashPassword, verifyPassword } from '../utils/bcryptUtil';
-import { asyncHandler, InternalServerError, UnauthorizedError, formatSuccessResponse} from '../utils/responseHandler';
+import { asyncHandler, InternalServerError, UnauthorizedError, formatSuccessResponse } from '../utils/responseHandler';
 import validateUserIdentity from '../services/tokenJwtService/validateUserIdentity';
 import prismaClient from '../services/prisma/prismaClient';
 import integerValidator from '../utils/integerValidator';
+
+interface UserDTO {
+    id: number;
+    email: string;
+    name: string;
+    course: Course;
+}
 
 /**
  * Get all users.
@@ -14,7 +21,7 @@ import integerValidator from '../utils/integerValidator';
  * @returns {Promise<void>}
  */
 export const getUsers = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-    const users = await prismaClient.user.findMany({
+    const users: UserDTO[] = await prismaClient.user.findMany({
         select: {
             id: true,
             name: true,
@@ -61,27 +68,29 @@ export const createUser = asyncHandler(async (req: Request, res: Response): Prom
 
     password = hash;
 
-    const user = await prismaClient.user.create({
+    const user: User = await prismaClient.user.create({
         data: {
             name,
             email,
             password,
             role,
             status,
-            courseId
+            courseId,
         },
     });
 
-    res.status(201).json(formatSuccessResponse({
-        user: {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            status: user.status,
-            courseId: user.courseId,
-        }
-    }));
+    res.status(201).json(
+        formatSuccessResponse({
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                status: user.status,
+                courseId: user.courseId,
+            },
+        })
+    );
 });
 
 /**
@@ -109,22 +118,19 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
             password: z.string().min(8).max(255).optional(),
             newPassword: z.string().min(8).max(255).optional(),
         })
-    .refine((data) => 
-    (data.email == data.newEmail) || (data.email && data.newEmail), {
-        message: 'Email cannot be updated as some data is missing',
-        path: ['email', 'newEmail'],
-    }
-    ).refine((data) =>
-        (data.password == data.newPassword) || (data.password && data.newPassword), {
+        .refine((data) => data.email == data.newEmail || (data.email && data.newEmail), {
+            message: 'Email cannot be updated as some data is missing',
+            path: ['email', 'newEmail'],
+        })
+        .refine((data) => data.password == data.newPassword || (data.password && data.newPassword), {
             message: 'Password cannot be updated as some data is missing',
-            path: ['password', 'newPassword']
-        }
-    );
+            path: ['password', 'newPassword'],
+        });
 
     const id = await integerValidator.parseAsync(req.body.tagId);
     const { newName, email, newEmail, newCourseId, password, newPassword } = await updateUserSchema.parseAsync(req.body);
 
-    const user = await prismaClient.user.findUniqueOrThrow({
+    const user: User = await prismaClient.user.findUniqueOrThrow({
         where: {
             id: id,
         },
@@ -143,7 +149,7 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
     const encriptedNewPassword = await hashPassword(newPassword, 11);
     if (!encriptedNewPassword) throw new InternalServerError('Unable to update password!');
 
-    const newUser = await prismaClient.user.update({
+    const newUser: User = await prismaClient.user.update({
         where: {
             id,
         },
@@ -155,14 +161,16 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
         },
     });
 
-    res.status(200).json(formatSuccessResponse({
-        user: {
-            id: newUser.id,
-            name: newUser.name,
-            email: newUser.email,
-            courseId: user.courseId,
-        }
-    }));
+    res.status(200).json(
+        formatSuccessResponse({
+            user: {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email,
+                courseId: user.courseId,
+            },
+        })
+    );
 };
 
 /**
@@ -175,19 +183,19 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
 export const deleteUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
     const id = await integerValidator.parseAsync(req.params.userId);
 
-    const user = await prismaClient.user.findUniqueOrThrow({
+    const user: User = await prismaClient.user.findUniqueOrThrow({
         where: {
             id,
-        }
+        },
     });
 
     // validating if the target user of the delete is the same as the token
     if (!validateUserIdentity(user.email, req.headers.authorization)) throw new UnauthorizedError('Unauthorized user');
 
-    const deletedUser = await prismaClient.user.delete({
+    const deletedUser: User = await prismaClient.user.delete({
         where: {
-            id: id
-        }
+            id: id,
+        },
     });
 
     res.status(200).json(formatSuccessResponse(deletedUser));
